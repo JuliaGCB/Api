215b005e1847b3501a754b09288c9593f3854f7a
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-02 20:02:10 -0300

    Add comentario

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,6 +8,6 @@ const notesController = new NotesController();
 
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
-notesRoutes.delete("/:id", notesController.delete);
+notesRoutes.delete("/:id", notesController.delete); //usando uma rota para deletar as notas
 
 module.exports = notesRoutes; //estou exportando esse arquivo para quem quiser usar// no caso outro arquivo
